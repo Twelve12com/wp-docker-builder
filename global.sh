1e8b95555d27b89ac72483c3fd90631a333ccd93
Bilal TAS <bilaltas@me.com>
2019-03-22 10:08:49 +0300

    Test

--- global.sh
+++ global.sh
@@ -134,7 +134,7 @@ function wp {
 function db_backup () {
 
 	# Save the DB backup
-	echo "Backing up the DB..."
+	echo "Backing up the DB... ($BASEDIR)"
 	DB_FILE=$BASEDIR/site/database/dump/wordpress_data.sql
 	docker-compose exec db /usr/bin/mysqldump -u root --password=password wordpress_data > ${DB_FILE}
 	tail -n +2 "${DB_FILE}" > "${DB_FILE}.tmp" && mv "${DB_FILE}.tmp" "${DB_FILE}"
