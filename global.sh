a1df93681e84f6a5b4a29e96d0d5ed3f85e1de21
Bilal TAS <bilaltas@me.com>
2019-05-21 17:05:38 +0300

    Auto detect URL changes

--- global.sh
+++ global.sh
@@ -208,9 +208,9 @@ function db_url_update () {
 
 
 	echo -e "Checking registered domain name..."
-	#OLD_DOMAIN="$(wp option get siteurl)" # DAMMIT BUG!
-	wp option get siteurl
-	read -ep "Write the URL above: " OLD_DOMAIN
+	OLD_DOMAIN="$(wp option get siteurl)"
+	#wp option get siteurl
+	#read -ep "Write the URL above: " OLD_DOMAIN
 	echo "Registered domain name: ${OLD_DOMAIN}"
 
 
